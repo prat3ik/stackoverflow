33e90a8edf37c0880322840b03d8ce4781ae337a
Pratik Patel <pratikpat@turn.com>
2017-01-03 16:56:22 +0530

    Test case added for get question which has max no. of count

--- src/pageobject/StackOveflowPO.java
+++ src/pageobject/StackOveflowPO.java
@@ -2,7 +2,9 @@ package pageobject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +12,14 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utils.WebDriverManager;
+
+/**
+ * This class contains Page Objects and methods for accessing web elements of
+ * Stackoverflow search page
+ * 
+ * @author pratikpat
+ *
+ */
 
 public class StackOveflowPO {
 
@@ -34,15 +44,11 @@ public class StackOveflowPO {
 	@FindBy(css = "a[rel='next']")
 	WebElement nextPage;
 
-	@FindBy(css = ".user-action-time .relativetime")
-	List<WebElement> datesAndTimes;
-
 	public void search(String keyword) {
 		searchBoxField.sendKeys(keyword + Keys.ENTER);
 	}
 
 	public void navigateToFrequentTab() {
-		Thread.sleep(5000);
 		frequentTab.click();
 	}
 
@@ -50,20 +56,144 @@ public class StackOveflowPO {
 		show50Items.click();
 	}
 
-	public void navigateToNextPage() {
-		if (nextPage.isDisplayed())
-			nextPage.click();
+	private void navigateToNextPage() {
+		nextPage.click();
 	}
 
-	public List<String> getDatesAndTimes() {
+	private boolean isNextPageAvailable() {
+		return isElementPresent(nextPage) && nextPage.isDisplayed();
+	}
+
+	private boolean isElementPresent(WebElement el) {
+		boolean isPresent = false;
+		try {
+			el.isDisplayed();
+			isPresent = true;
+		} catch (Exception e) {
+			isPresent = false;
+		}
+		return isPresent;
+	}
+
+	List<String> datesAndTimesTextForAllPages = new ArrayList<String>();
+	List<String> viewCountsForAllPages = new ArrayList<String>();
+	List<String> questionLinkForAllPages = new ArrayList<String>();
+
+	private List<WebElement> questionSummary() {
+		final String questionSummaryCss = ".question-summary";
+		List<WebElement> dateAndTimeElement = getDriver().findElements(By.cssSelector(questionSummaryCss));
+		return dateAndTimeElement;
+	}
+
+	private List<String> getDatesAndTimesForSinglePage() {
+		List<WebElement> questionSummary = questionSummary();
 		List<String> datesAndTimesText = new ArrayList<String>();
-		for (WebElement el : datesAndTimes) {
-			datesAndTimesText.add(el.getText());
+		final String dateAndTimeCss = ".user-action-time .relativetime";
+
+		if (questionSummary.size() > 0) {
+			for (WebElement el : questionSummary) {
+				String dateAndTimeText;
+				try {
+					dateAndTimeText = el.findElement(By.cssSelector(dateAndTimeCss)).getText();
+				} catch (Exception e) {
+					dateAndTimeText = 0 + "";
+				}
+				datesAndTimesText.add(dateAndTimeText);
+			}
 		}
 		return datesAndTimesText;
+	}
+
+	private List<String> getViewCountsForSinglePage() {
+		List<WebElement> questionSummary = questionSummary();
+		List<String> viewCountText = new ArrayList<String>();
+		final String viewsCountCss = ".statscontainer .views";
+
+		if (questionSummary.size() > 0) {
+			for (WebElement el : questionSummary) {
+				viewCountText.add(el.findElement(By.cssSelector(viewsCountCss)).getText());
+			}
+		}
+		return viewCountText;
+	}
+
+	private List<String> getQuestionLinksForSinglePage() {
+		List<WebElement> questionSummary = questionSummary();
+		List<String> questionLinkText = new ArrayList<String>();
+
+		final String questionLinkCss = "h3 .question-hyperlink";
+		if (questionSummary.size() > 0) {
+			for (WebElement el : questionSummary) {
+				questionLinkText.add(el.findElement(By.cssSelector(questionLinkCss)).getText());
+			}
+		}
+		return questionLinkText;
+	}
+
+	private void calcaulateDataForAllPage() {
+		getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		datesAndTimesTextForAllPages.addAll(getDatesAndTimesForSinglePage());
+		viewCountsForAllPages.addAll(getViewCountsForSinglePage());
+		questionLinkForAllPages.addAll(getQuestionLinksForSinglePage());
+		while (isNextPageAvailable()) {
+			navigateToNextPage();
+			getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			datesAndTimesTextForAllPages.addAll(getDatesAndTimesForSinglePage());
+			viewCountsForAllPages.addAll(getViewCountsForSinglePage());
+			questionLinkForAllPages.addAll(getQuestionLinksForSinglePage());
+		}
+	}
+
+	public List<Integer> getCurrentYearQuestionsIndexes(String year) {
+		calcaulateDataForAllPage();
+		List<Integer> indexsForCurrentYear = new ArrayList<Integer>();
+		String calculationYear = year.substring(Math.max(year.length() - 2, 0));
+		for (int i = 0; i < datesAndTimesTextForAllPages.size(); i++) {
+			// System.out.println(i + " " + datesAndTimesTextForAllPages.get(i)
+			// + " count: "
+			// + viewCountsForAllPages.get(i) + " quest: " +
+			// questionLinkForAllPages.get(i));
+			if (datesAndTimesTextForAllPages.get(i).contains("'" + calculationYear + " at ")) {
+				indexsForCurrentYear.add(i);
+			}
+		}
+		return indexsForCurrentYear;
+	}
+
+	private int getMaxViewsIndex(List<Integer> indexes) {
+		int maxViewIndex = 0;
+		long maxViewCount = getNoOfViews(indexes.get(maxViewIndex));
+
+		for (int i = 1; i < indexes.size(); i++) {
+			if (getNoOfViews(indexes.get(i)) > maxViewCount) {
+				maxViewIndex = indexes.get(i);
+				maxViewCount = getNoOfViews(indexes.get(i));
+			}
+		}
+		return maxViewIndex;
+	}
+
+	public void moveToMaxViewedQuestion(String year) {
+		int index = getMaxViewsIndex(getCurrentYearQuestionsIndexes(year));
+		System.out.println("Most Asked question for " + year + "year : " + questionLinkForAllPages.get(index));
+		System.out.println("Views : " + viewCountsForAllPages.get(index));
+	}
+
+	private long getNoOfViews(int index) {
+		long totalViews = 0;
+		String viewCountString = viewCountsForAllPages.get(index).split("\\s+")[0];
+		if (viewCountString.contains("k")) {
+			long viewCount = Long.parseLong(viewCountString.split("k")[0]);
+			// 'k' means 1000
+			totalViews = viewCount * 1000;
+		} else {
+			totalViews = Long.parseLong(viewCountString);
+		}
+		return totalViews;
 	}
 
 	public void navigateToStackOverflow() {
 		getDriver().get("http://stackoverflow.com/");
 	}
+
 }
--- src/testcase/StackOveflow.java
+++ src/testcase/StackOveflow.java
@@ -1,30 +1,51 @@
 package testcase;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import pageobject.StackOveflowPO;
-
+import utils.WebDriverManager;
+/**
+ * This class contains test method which calculate the most asked question for tag:'qa' in current year
+ * 
+ * @author pratikpat
+ *
+ */
 public class StackOveflow {
-	public static void main(String[] args) {
-		System.out.println("hey");
-	}
-	
+
+	//For current year(2017)there are no questions for tag:[qa], so the previous year (2016) is selected
 	@Test
-	public void mtest(){
+	public void verifyMostFreqeuntQuestionAskedInThisYearForPaticularTag() {
+		final String tagName = "[qa]";
+		final String calcaulationYear = "2016";
 		StackOveflowPO po = new StackOveflowPO();
 		po.navigateToStackOverflow();
-		po.search("[qa]");
+		po.search(tagName);
 		po.navigateToFrequentTab();
 		po.show50ListItems();
-		System.out.println(po.getDatesAndTimes());
+		po.moveToMaxViewedQuestion(calcaulationYear);
 	}
-	
-//
-//	@BeforeMethod
-//	public void beforeMethod() {
-//	}
 
+	@AfterMethod
+	public void quitWebDriver(ITestResult testResult) {
+		if (testResult.FAILURE == testResult.getStatus()) {
+			File src = ((TakesScreenshot) WebDriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+			try {
+				// now copy the screenshot to desired location using copyFile method
+				FileUtils.copyFile(src, new File("C:/selenium/error.png"));
+			}
+			catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		WebDriverManager.getDriver().quit();
+	}
 
 }
